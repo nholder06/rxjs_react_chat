d601f021b1ef90adf32eb4d4e5eec961fb47e160
Nicole Holder <holdern@wwt.com>
2020-10-19 18:02:03 -0500

    added methods to chatStore, also added additional properties to initialState

--- src/store/chat.js
+++ src/store/chat.js
@@ -1,15 +1,32 @@
 import { Subject } from 'rxjs';
 
 const subject = new Subject();
-
 const initialState = {
+    status: '',
     data: [],
-    newDataContent: 0,
+    newDataCount: 0,
+    error: ''
 };
 
 let state = initialState;
 
 const chatStore = {
-    init: () => subject.next(state),
-    subscribe: setState => subject.subscribe(setState)
-}
+    init: () => {
+    <b>state = {...state, newDataCount= 0},</b>
+    subject.next(state)
+    },
+    subscribe: setState => subject.subscribe(setState),
+    sendMessage: message => {
+        state = {
+            ...state,
+            data: [...state.data, message],
+            newDataContent: state.newDataCount + 1
+        };
+        subject.next(state);
+    },
+    clearChat: () => {
+        state = initialState;
+        subject.next(state);
+    },
+    initialState
+};
