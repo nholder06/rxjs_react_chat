e69e95912fa18df708356d4194453c4766be808c
Nicole Holder <holdern@wwt.com>
2020-10-21 09:27:26 -0500

    added html and onFormSubmit botton for firstPerson

--- src/components/FirstPerson.js
+++ src/components/FirstPerson.js
@@ -8,4 +8,44 @@ const FirstPerson = () => {
         chatStore.subscribe(setChatState);
         chatStore.init();
     }, []);
+
+    const onFormSubmit = e => {
+        e.preventDefault();
+        const messageObject = {
+            person: 'first-person',
+            text: e.target.elements.messageInput.value.trim(),
+        };
+        chatStore.sendMessage(messageObject);
+        document.getElementById('messageForm').requestFullscreen();
+    };
+
+    return (
+        <div className='container'>
+            <h2>Mycroft</h2>
+            <div className='chat-box'>
+                {chatState.data.map(message => (
+                    <div>
+                    <p className={message.person}>{message.text}</p>
+                    <div className='clear'></div>
+                    </div>
+                ))}
+            </div>
+            <form id='messageForm' onSubmit={onFormSubmit}>
+                <input
+                type='text'
+                id='messageInput'
+                name='messageInput'
+                placeholder='type here...'
+                required
+            />
+            <button
+            type='submit'>Send</button>
+            </form>
+            <button className='clear-button' onClick={() => chatStore.clearChat()}>
+                Clear Chat
+            </button>
+        </div>
+    );
 }
+
+export default FirstPerson;
